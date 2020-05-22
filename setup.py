cda8606d62073ba0e97f1ac33a4ed36afd1a451c
ganjigajanan <ganji.gajanan@gmail.com>
2020-05-22 15:32:07 -0500

    pointing gen3dictionary to specific branch

--- setup.py
+++ setup.py
@@ -11,12 +11,15 @@ setup(
         "rstr==2.2.6",
         # "dictionaryutils~=3.0.2",
         "dictionaryutils",
-        "gen3dictionary~=2.0.1",
+        # "gen3dictionary~=2.0.1",
+        "gen3dictionary",
     ],
     dependency_links=[
         "git+https://github.com/uc-cdis/cdislogging.git@1.0.0#egg=cdislogging",
         # for testing purpose
         "git+https://github.com/uc-cdis/dictionaryutils.git@feat/format_upgrade#egg=dictionaryutils",
+        "git+https://github.com/uc-cdis/datadictionary.git@feat/format_upgrade#egg=gen3dictionary",
+
     ],
     scripts=["bin/data-simulator"],
 )
