c9e39b954e7047613f6097df41ccac0ab5eb33e5
Albert Snow <ajsnow2012@gmail.com>
2024-03-27 16:53:23 -0500

    fix(test): trying to add tests but it's complaining

---
+++ tests/test_file_handling.py
@@ -0,0 +1,13 @@
+from datasimulator.graph import Graph
+from dictionaryutils import dictionary
+from datasimulator.file_handling import write_to_file_or_log_error
+
+
+def test_write_to_file_or_log_error():
+    graph = Graph(dictionary, "DEV", "test")
+    graph.generate_nodes_from_dictionary()
+    graph.construct_graph_edges()
+    submission_order = graph.generate_submission_order()
+    file_path = "test-da/DataImportOrderPath.txt"
+    output = write_to_file_or_log_error(file_path, submission_order)
+    assert False
