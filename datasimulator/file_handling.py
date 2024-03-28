b251929edf85ae73ec9d7c681755eb716a9c671b
Albert Snow <ajsnow2012@gmail.com>
2024-03-28 11:47:42 -0500

    fix(function): finalizing file writer

--- datasimulator/file_handling.py
+++ datasimulator/file_handling.py
@@ -1,19 +1,20 @@
+from .utils import attempt  # same as datasimulator.utils
 from cdislogging import get_logger
 logger = get_logger("data-simulator", log_level="info")
-from .utils import attempt # same as datasimulator.utils
+
 
 def write_node_to_file(outfile, node):
     outfile.write(f"{node.name}\t{node.category}\n")
 
 
 def write_to_file_or_log_error(file_path, submission_order):
-    with open(file_path, "w") as output_file:
-        file_open_result = {} # attempt(lambda: open(file_path, "w"))
-        if not file_open_result["success"]:
-            exception = file_open_result["error"]
-            logger.error(f"Error trying to open a new file DataImportOrderPath. Error: {exception}")
-        else:
-            output_file = file_open_result["result"]
-            for node in submission_order:
-                write_node_to_file(output_file, node)
-            logger.info("DataImportOrderPath.txt has been successfully created.")
+    file_open_result = attempt(lambda: open(file_path, "w"))
+    if not file_open_result["success"]:
+        exception = file_open_result["error"]
+        logger.error(f"Error trying to open a new file DataImportOrderPath. Error: {exception}")
+    else:
+        output_file = file_open_result["result"]
+        for node in submission_order:
+            write_node_to_file(output_file, node)
+        logger.info("DataImportOrderPath.txt has been successfully created.")
+    return file_open_result["success"]
