5c2a1b20ff07b737645d3e6cf65e7110ef7953b0
Albert Snow <ajsnow2012@gmail.com>
2024-03-28 15:58:42 -0500

    fix(refactor): refactor attempt 1, currying

--- datasimulator/file_handling.py
+++ datasimulator/file_handling.py
@@ -1,17 +1,31 @@
-from .utils import attempt  # same as datasimulator.utils
 from cdislogging import get_logger
 logger = get_logger("data-simulator", log_level="info")
 
 
-def write_submission_order_to_file_or_log_error(file_path, submission_order):
-    file_open_result = attempt(lambda: open(file_path, "w"))
-    if not file_open_result["success"]:
-        exception = file_open_result["error"]
-        logger.error(f"Error trying to open a new file '{file_path}'. Error: {exception}")
-    else:
-        output_file = file_open_result["result"]
+def try_writing_to_file(file_path, action):
+    """
+    Args:
+        file_path (str): alleged path to file
+        action (callable): whatever you want to do with the open file
+
+    Returns: result dict in the form
+        - "success" (bool): whether it succeeded
+        - "result": action return if successful
+        - "error": exception if failed
+    """
+    try:
+        output_file = open(file_path, "w")
+        result = action(output_file)
+        output_file.close()
+        return {"success": True, "result": result}
+    except Exception as e:
+        logger.error(f"Error trying to open a new file '{file_path}'. Error: {e}")
+        return {"success": False, "result": e}
+
+
+def write_submission_order_to_file(submission_order):
+    def write_node_to_file(output_file):
         for node in submission_order:
             output_file.write(f"{node.name}\t{node.category}\n")
-        output_file.close()
-        logger.info("DataImportOrderPath.txt has been successfully created.")
-    return file_open_result["success"]
+    return write_node_to_file
+
