53f22acbfc4fc92c098af49a6d47feb2d92c5e69
Albert Snow <ajsnow2012@gmail.com>
2024-03-28 11:48:01 -0500

    fix(test): finalizing test

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,13 +1,15 @@
+import os
 from datasimulator.graph import Graph
 from dictionaryutils import dictionary
 from datasimulator.file_handling import write_to_file_or_log_error
-from datasimulator.utils import attempt
 
-def test_write_to_file_or_log_error():
+
+def test_write_to_file_or_log_error(tmpdir):
+    """ asking for tmpdir in the parameter provides an empty test directory for testing """
     graph = Graph(dictionary, "DEV", "test")
     graph.generate_nodes_from_dictionary()
     graph.construct_graph_edges()
     submission_order = graph.generate_submission_order()
-    file_path = "test-da/DataImportOrderPath.txt"
-    output = write_to_file_or_log_error(file_path, submission_order)
-    assert False
+    test_path = tmpdir.mkdir("test-data").join("DataImportOrderPath.txt")
+    succeeded = write_to_file_or_log_error(test_path, submission_order)
+    assert succeeded and os.path.getsize(test_path) > 0
