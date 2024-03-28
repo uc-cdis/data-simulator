e6f70e823356d657cb28b04378793d109c27c825
Albert Snow <ajsnow2012@gmail.com>
2024-03-28 14:35:28 -0500

    fix(refactor): hard coding test

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -1,15 +1,21 @@
 import os
-from datasimulator.graph import Graph
-from dictionaryutils import dictionary
 from datasimulator.file_handling import write_to_file_or_log_error
+from datasimulator.node import Node
 
 
 def test_write_to_file_or_log_error(tmpdir):
     """ asking for tmpdir in the parameter provides an empty test directory for testing """
-    graph = Graph(dictionary, "DEV", "test")
-    graph.generate_nodes_from_dictionary()
-    graph.construct_graph_edges()
-    submission_order = graph.generate_submission_order()
+    node_a = Node(
+        'first_node',
+        {"category": "foo", "properties": "bar", "links": "biz", "oneOf": "baz"},
+        'test_project',
+        False)
+    node_b = Node(
+        'second_node',
+        {"category": "aoo", "properties": "aar", "links": "aiz", "oneOf": "aaz"},
+        'test_project',
+        False)
+    submission_order = [node_a, node_b]
     test_path = tmpdir.mkdir("test-data").join("DataImportOrderPath.txt")
     succeeded = write_to_file_or_log_error(test_path, submission_order)
     assert succeeded and os.path.getsize(test_path) > 0
