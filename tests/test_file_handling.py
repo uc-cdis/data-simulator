d0f148f377c1cf04d7b9bb17f1696015623441ff
Albert Snow <ajsnow2012@gmail.com>
2024-04-01 11:36:53 -0500

    fix(testing): ensure content is as expected

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -18,11 +18,17 @@ node_b = Node(
 example_submission_order = [node_a, node_b]
 
 
+def get_contents(test_path):
+    with open(test_path, "r") as file:
+        contents = file.read()
+        return contents
+
+
 def test_write_to_file_or_log_error_succeeds(tmpdir):
     """ asking for tmpdir in the parameter provides an empty test directory for testing """
     test_path = tmpdir.mkdir("test-data").join("DataImportOrderPath.txt")
     succeeded = write_submission_order_to_file(example_submission_order, test_path)
-    assert succeeded and os.path.getsize(test_path) > 0
+    assert succeeded and os.path.getsize(test_path) > 0 and get_contents(test_path) == 'first_node\tfoo\nsecond_node\taoo\n'
 
 
 def test_write_to_file_or_log_error_fails():
