da03845e14a23f0175d2cf5ed3a72e2140fdb7ee
Albert Snow <ajsnow2012@gmail.com>
2024-04-01 11:38:28 -0500

    fix(docs): remove submissions

--- datasimulator/file_handling.py
+++ datasimulator/file_handling.py
@@ -3,7 +3,7 @@ logger = get_logger("data-simulator", log_level="info")
 
 
 def write_submission_order_to_file(submission_order, file_path):
-    """ given a correct file path, writes a name-category pair to the file from a list of nodes (submissions) """
+    """ given a correct file path, writes a name-category pair to the file from a list of nodes """
     with open(file_path, "w") as output_file:
         for node in submission_order:
             output_file.write(f"{node.name}\t{node.category}\n")
